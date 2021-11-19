861928221aead3b3ee0fb0335149b81d1a47ff34
Felipe Alan <ficlek13@hotmail.com>
2021-11-19 00:00:10 -0300

    Primeiro javascript

---
+++ hobby-javascript/hobby.js
@@ -0,0 +1,12 @@
+document.getElementById('h1-nome').innerHTML = "Felipe Alan de Almeida";
+document.getElementById('h3-info').innerHTML = "23 anos e moro no estado de São Paulo";
+document.getElementById('img-perfil').src = "images/foto.jpg";
+document.getElementById('h2-hobby').innerHTML = "Meu Hobby é jogar qualquer jogo em alto nível";
+document.getElementById('p-descri').innerHTML = "Gosto de jogos extremamente competitivos, como League of Legends, Starcraft2, Call of Duty Warzone, Overwatch, Warthunder, Rainbow Six Siege.";
+document.getElementById('link-1').href = "https://www.leagueoflegends.com/pt-br/";
+document.getElementById('link-2').href = "https://starcraft2.com/pt-br/";
+document.getElementById('link-3').href = "https://www.callofduty.com/content/atvi/callofduty/warzone/web/pt_br/home.html";
+document.getElementById('link-4').href = "https://playoverwatch.com/pt-br/";
+document.getElementById('link-5').href = "https://warthunder.com/br";
+document.getElementById('link-6').href = "https://www.ubisoft.com/pt-br/game/rainbow-six/siege";
+
