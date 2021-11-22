4baf9e01a6b848281943136ba40885a47a64a14f
Felipe Alan <ficlek13@hotmail.com>
2021-11-22 00:02:47 -0300

    banda

---
+++ banda/java.js
@@ -0,0 +1,18 @@
+// querySelector
+let perfil = document.querySelector(".perfil");
+let apres = document.querySelector(".apres");
+let banda1= document.querySelector(".banda1");
+let banda2= document.querySelector(".banda2");
+
+
+// Constantes
+const one = 300;
+const two = 500;
+// CSS dos elementos
+perfil.style.width = one+"px";
+perfil.style.height = one+"px";
+perfil.style.borderRadius = one+"px";
+banda1.style.width = one+"px";
+banda1.style.height = one+"px";
+banda2.style.width = two+"px";
+banda2.style.height = one+"px";
